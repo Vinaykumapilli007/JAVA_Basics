5b076e8ec6d0e977d7f52dd6275992f7117b9ae1
Vinaykumapilli007 <113400056+Vinaykumapilli007@users.noreply.github.com>
2023-02-13 20:35:46 +0530

    Add files via upload

---
+++ classB.java
@@ -0,0 +1,27 @@
+package pack1;
+
+public class classB
+{
+	void main1()
+	{
+		 int a=10;
+		 System.out.println("a value is :"+a);
+		 System.out.println(90-a);
+		 classB aobj=new class();
+		 aobj.meth2();
+		 System.out.println(1000);
+	}
+	void meth2()
+	{
+		int a=99;
+		System.out.println("meth2 called");
+		System.out.println("addition value===>"+(a=1));
+	}
+	public static void main(String[] args)
+	{
+		System.out.println("hi");
+		classB aobj= new class();
+		aobj.meth1();
+		System.out.println("end");
+	}
+ }
